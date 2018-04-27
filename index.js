7d9ab6d2be9263cb36f299f4eef504ca57560efe
Michael Eby <michaeleby1@gmail.com>
2018-04-27 00:48:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+
+  for (let i = 0; i < musicians.length; i++) {
+  array.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+  return array
+}
+
+function johnLennonFacts(facts) {
+  let facts2 = []
+  let i = 0
+  while (i <= facts.length - 1) {
+  facts2.push(`${facts[i++]}!!!`)
+}
+  return facts2
+}
+
+function iLoveTheBeatles(number) {
+  let array = []
+  let i = 0
+  do {
+  array.push('I love the Beatles!')
+} while (i < 15)
+return array
+}
